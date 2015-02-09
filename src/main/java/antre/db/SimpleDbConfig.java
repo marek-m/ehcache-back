b7adcb0e26abc91b6dd7f49620e7f50a798c2830
Marek <marek>
2015-02-09 15:14:40 +0100

    db test

---
+++ src/main/java/antre/db/SimpleDbConfig.java
@@ -0,0 +1,42 @@
+package antre.db;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SimpleDbConfig {
+
+    @Bean
+    public DataSource dataSource()  {    
+
+        URI dbUri;
+        try {
+            String username = "username";
+            String password = "password";
+            String url = "jdbc:postgresql://localhost/dbname";
+            String dbProperty = System.getProperty("database.url");
+            if(dbProperty != null) {
+                dbUri = new URI(dbProperty);
+
+                username = dbUri.getUserInfo().split(":")[0];
+                password = dbUri.getUserInfo().split(":")[1];
+                url = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            }     
+
+            BasicDataSource basicDataSource = new BasicDataSource();
+            basicDataSource.setUrl(url);
+            basicDataSource.setUsername(username);
+            basicDataSource.setPassword(password);
+            return basicDataSource;
+
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+}
